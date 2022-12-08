333f026f1b6623df0df574deb23a9cc4397aada3
nate285 <nhoag@seas.upenn.edu>
2022-12-07 23:19:34 -0500

    nice

---
+++ client.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+
+int main(int argc, char *argv[])
+{
+    char *host_addr = argv[1];
+    int port = atoi(argv[2]);
+
+    /* Open a socket */
+    int s;
+    if ((s = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("simplex-talk: socket");
+        exit(1);
+    }
+
+    /* Config the server address */
+    struct sockaddr_in sin;
+    sin.sin_family = AF_INET;
+    sin.sin_addr.s_addr = inet_addr(host_addr);
+    sin.sin_port = htons(port);
+    // Set all bits of the padding field to 0
+    memset(sin.sin_zero, '\0', sizeof(sin.sin_zero));
+
+    /* Connect to the server */
+    if (connect(s, (struct sockaddr *)&sin, sizeof(sin)) < 0)
+    {
+        perror("simplex-talk: connect");
+        close(s);
+        exit(1);
+    }
+    char recBuf[200];
+    int recLen = recv(s, recBuf, sizeof(recBuf), 0);
+    fputs(recBuf, stdout);
+    fputc('\n', stdout);
+    fflush(stdout);
+
+    size_t buf_size = 200;
+    char *buf;
+    buf = malloc(sizeof(char) * 200);
+    getline(&buf, &buf_size, stdin);
+    int len = strlen(buf) + 1;
+    send(s, buf, len, 0);
+}
